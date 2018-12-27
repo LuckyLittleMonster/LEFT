9c1e653b4dbc2a630e5a48524de443e70f033e86
Huanyi Qin <hqin4@binghamton.edu>
2018-12-27 11:21:02 -0500

    1

--- app.py
+++ app.py
@@ -54,4 +54,4 @@ def upload_file():
 
 if __name__=="__main__":
     app.secret_key = 'supersecretkey'
-    app.run(debug=True, host='0.0.0.0', port=5000)
+    app.run(debug=True, host='0.0.0.0', port=80)
